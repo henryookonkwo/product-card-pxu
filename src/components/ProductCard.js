b193f2a6a7977f9b8cbac82a2f1d9fd94a82f520
Henry Okonkwo <47309196+henryookonkwo@users.noreply.github.com>
2024-08-29 20:00:59 -0400

    Add Sold-Out functionality

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -3,9 +3,15 @@ import Rating from "./Rating";
 
 const ProductCard = ({ title, price, image, addToCart }) => {
   const [rating, setRating] = useState(0);
+  const [isAddedToCart, setIsAddedToCart] = useState(false);
 
   const handleRating = (rate) => {
     setRating(rate);
+  };
+
+  const handleAddToCart = () => {
+    addToCart({ title, price, image, rating });
+    setIsAddedToCart(true);
   };
 
   return (
@@ -16,15 +22,30 @@ const ProductCard = ({ title, price, image, addToCart }) => {
           alt="Product"
           className="w-full h-48 object-cover rounded-lg group-hover:opacity-50 transition-opacity duration-300"
         />
-        <button
-          onClick={addToCart}
-          className="absolute inset-0 m-auto flex items-center justify-center w-32 h-10 text-white bg-blue-500 rounded-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300"
-        >
-          Add to Cart
-        </button>
+        <div></div>
+        {isAddedToCart && (
+          <span className="absolute bottom-0 left-0 m-2 px-2 py-1 text-xs font-semibold text-white bg-amber-400 rounded-full">
+            Sold <br />
+            Out
+          </span>
+        )}
+        {!isAddedToCart && (
+          <button
+            onClick={handleAddToCart}
+            className="absolute inset-0 m-auto flex items-center justify-center w-32 h-10 text-white bg-blue-500 rounded-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300"
+          >
+            Add to Cart
+          </button>
+        )}
       </div>
       <div className="flex flex-col items-center justify-center h-full">
-        <h2 className="mt-4 text-xl font-semibold">{title}</h2>
+        <h2
+          className={`mt-4 text-xl font-semibold ${
+            isAddedToCart ? "line-through text-gray-500" : ""
+          }`}
+        >
+          {title}
+        </h2>
         <p className="mt-2 text-gray-500">{price}</p>
       </div>
       <Rating rating={rating} handleRating={handleRating} />
